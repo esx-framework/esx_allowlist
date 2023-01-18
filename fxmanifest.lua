7c657c6cb4837242e89ce87962236e73f1fa9640
Benzo <102178921+Benzo00@users.noreply.github.com>
2023-01-18 14:35:21 +0100

    feat - new repos

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'ESX-Framework'
+description 'Offical AllowList script for ESX'
+
+version '1.0' 
+legacyversion '1.9.1'
+
+lua54 'yes'
+server_only 'yes'
+
+server_scripts {
+	'@es_extended/imports.lua',
+	'@es_extended/locale.lua',
+	'config.lua',
+	'locales/*.lua',
+	'server/main.lua'
+}
