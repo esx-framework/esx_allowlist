47df145e0ab4184ba32be502a5063a9956eeb084
tayer <ruzetayer@gmail.com>
2023-02-24 08:02:44 -0500

    Create zh-cn.lua
    Add Simplified Chinese translation

---
+++ locales/zh-cn.lua
@@ -0,0 +1,9 @@
+
+Locales['en'] = {
+  ['allowlist_check']     = '检查您是否得到白名单批准.',
+  ['not_allowlisted']     = '您必须得到白名单批准方可加入此服务器!',
+  ['allowlist_empty']     = '该服务器没有开启白名单.',
+  ['license_missing']     = '错误：您的标识符丢失或未找到了!',
+  ['help_allowlist_add']  = '将某人添加到白名单中',
+  ['help_allowlist_load'] = '重新加载白名单',
+}
