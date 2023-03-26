600897e18a934fcfa26415be50a62edf8894a4da
Blooper <122776166+Blo0per@users.noreply.github.com>
2023-03-26 20:28:34 +0300

    Create fi.lua
    ### _Here is fi.lua for esx_allowlist_

---
+++ locales/fi.lua
@@ -0,0 +1,9 @@
+
+Locales['fi'] = {
+  ['allowlist_check']     = 'Tarkistetaan onko sinulla oleskelulupaa.',
+  ['not_allowlisted']     = 'Sinun on oltava oleskelulupa, jotta voit liittyä palvelimelle!', 
+  ['allowlist_empty']     = 'Tälle palvelimelle ei ole tallennettuja oleskelulupia.', 
+  ['license_missing']     = 'Virhe: Tunistettasi ei löydetä!',
+  ['help_allowlist_add']  = 'Lisää jollekulle oleskelulupa',
+  ['help_allowlist_load'] = 'Lataa oleskelulupa uudelleen',
+}
