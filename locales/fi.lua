9886146ce50267ff7eac4402803af3c59892799c
Blooper <122776166+Blo0per@users.noreply.github.com>
2023-03-26 20:32:06 +0300

    Update fi.lua

--- locales/fi.lua
+++ locales/fi.lua
@@ -1,6 +1,6 @@
 Locales['fi'] = {
   ['allowlist_check']     = 'Tarkistetaan onko sinulla oleskelulupaa.',
-  ['not_allowlisted']     = 'Sinun on oltava oleskelulupa, jotta voit liittyä palvelimelle!', 
+  ['not_allowlisted']     = 'Sinulla on oltava oleskelulupa, jotta voit liittyä palvelimelle!', 
   ['allowlist_empty']     = 'Tälle palvelimelle ei ole tallennettuja oleskelulupia.', 
   ['license_missing']     = 'Virhe: Tunistettasi ei löydetä!',
   ['help_allowlist_add']  = 'Lisää jollekulle oleskelulupa',
