0b6856c40ceef601e264a4494594cec8f2c6a2c5
Nemanja <71012671+NeMyOriginal@users.noreply.github.com>
2023-04-02 12:04:23 +0200

    Update sr.lua
    fixin typos and added šćčđć characters

--- locales/sr.lua
+++ locales/sr.lua
@@ -1,8 +1,8 @@
 Locales['sr'] = {
   ['allowlist_check']     = 'Proveravam da li ste na listi dozvoljenih.',
   ['not_allowlisted']     = 'Morate biti na listi dozvoljenih na serveru!',
-  ['allowlist_empty']     = 'Nema sacuvanih lista dozvoljenih za ovaj server.',
-  ['license_missing']     = 'Greska: Vas identifikator nedostaje!',
-  ['help_allowlist_add']  = 'dodajte nekoga na listu dozvoljenih igraca',
-  ['help_allowlist_load'] = 'ponovo ucitajte listu dozvoljenih igraca',
+  ['allowlist_empty']     = 'Nema sačuvanih lista dozvoljenih za ovaj server.',
+  ['license_missing']     = 'Greška: Vaš identifikator nedostaje!',
+  ['help_allowlist_add']  = 'dodajte nekoga na listu dozvoljenih igrača',
+  ['help_allowlist_load'] = 'ponovo učitajte listu dozvoljenih igrača',
 }
