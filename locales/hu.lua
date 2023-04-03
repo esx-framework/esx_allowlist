32d1a5c3dd951114c2aedd88c75aa0a30d54aeea
pecedevelopment <97826642+pecedevelopment@users.noreply.github.com>
2023-04-03 17:45:47 +0200

    Update hu.lua

--- locales/hu.lua
+++ locales/hu.lua
@@ -4,5 +4,11 @@ Locales['hu'] = {
   ['allowlist_empty']     = 'Ehhez a szerverhez nincsenek mentett engedélyezési listák.',
   ['license_missing']     = 'Hiba: Te azonosítód hiányzik!',
   ['help_allowlist_add']  = 'játékos felvétele az engedélyezettek listájára',
+  ['help_allowlist_remove'] = 'játékos törlése az engedélyezettek listájáról',
   ['help_allowlist_load'] = 'engedélyezési lista újratöltése',
+  ['error'] = 'Hiba történt, keresd fel a szerver tulajdonosát!', 
+  ['already_allowlisted'] = 'A játékos már engedélyezett!', 
+  ['license'] = 'licenc',
+  ['help_license'] = 'játékos  licenc', 
+  ['identifier_not_allowlisted'] = 'A licenc nem engedélyezett a szerveren!', 
 }
