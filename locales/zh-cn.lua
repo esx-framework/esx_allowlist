4ccfaa47bd6dc4554acdb2cfeab27d2571bfce40
pecedevelopment <97826642+pecedevelopment@users.noreply.github.com>
2023-04-03 18:31:20 +0200

    Update zh-cn.lua

--- locales/zh-cn.lua
+++ locales/zh-cn.lua
@@ -5,4 +5,10 @@ Locales['zh-cn'] = {
   ['license_missing']     = '错误：您的标识符丢失或未找到了!',
   ['help_allowlist_add']  = '将某人添加到白名单中',
   ['help_allowlist_load'] = '重新加载白名单',
+  ['help_allowlist_remove'] = 'remove someone from the allowlist', --not translated
+  ['error'] = 'There Was An Error, Please Contact the server owner!', --not translated
+  ['already_allowlisted'] = 'The player is already allowlisted on this server!', --not translated
+  ['license'] = 'license', --not translated
+  ['help_license'] = 'the player license', --not translated
+  ['identifier_not_allowlisted'] = 'Identifier is not Allowlisted on this server!', --not translated
 }
