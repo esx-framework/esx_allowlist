4d332017f33bc4295bc5e9486dd93eaf17a6b3db
TheFantomas <117121911+TheFantomas@users.noreply.github.com>
2023-04-05 00:58:10 +0200

    Hm

--- locales/nl.lua
+++ locales/nl.lua
@@ -5,10 +5,10 @@ Locales['nl'] = {
   ['license_missing']     = 'Error: Je Identifier mist!',
   ['help_allowlist_add']  = 'geef iemand een toegangspas',
   ['help_allowlist_load'] = 'herlaad de toegangspassen',
-  ['help_allowlist_remove'] = 'verwijder iemand zijn toegangspas', --not translated
-  ['error'] = 'Er is een fout opgetreden, contacteer de server eigenaar !', --not translated
-  ['already_allowlisted'] = 'De speler heeft al een toegangspas op deze server!', --not translated
-  ['license'] = 'licentie', --not translated
-  ['help_license'] = 'de speler zijn licentie', --not translated
-  ['identifier_not_allowlisted'] = 'Deze identifier heeft geen toegangspas op deze server!', --not translated
+  ['help_allowlist_remove'] = 'verwijder iemand zijn toegangspas',
+  ['error'] = 'Er is een fout opgetreden, contacteer de server eigenaar !',
+  ['already_allowlisted'] = 'De speler heeft al een toegangspas op deze server!',
+  ['license'] = 'licentie',
+  ['help_license'] = 'de speler zijn licentie',
+  ['identifier_not_allowlisted'] = 'Deze identifier heeft geen toegangspas op deze server!',
 }
