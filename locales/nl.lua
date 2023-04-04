bbdd07dde806f47d2830be6d90caae8cc0913971
Arctos2win <116841243+Arctos2win@users.noreply.github.com>
2023-04-04 22:34:09 +0200

    Update nl.lua
    Translating new locales

--- locales/nl.lua
+++ locales/nl.lua
@@ -5,10 +5,10 @@ Locales['nl'] = {
   ['license_missing']     = 'Error: Je Identifier mist!',
   ['help_allowlist_add']  = 'geef iemand een toegangspas',
   ['help_allowlist_load'] = 'herlaad de toegangspassen',
-  ['help_allowlist_remove'] = 'remove someone from the allowlist', --not translated
-  ['error'] = 'There Was An Error, Please Contact the server owner!', --not translated
-  ['already_allowlisted'] = 'The player is already allowlisted on this server!', --not translated
-  ['license'] = 'license', --not translated
-  ['help_license'] = 'the player license', --not translated
-  ['identifier_not_allowlisted'] = 'Identifier is not Allowlisted on this server!', --not translated
+  ['help_allowlist_remove'] = 'verwijder iemand zijn toegangspas', --not translated
+  ['error'] = 'Er is een fout opgetreden, contacteer de server eigenaar !', --not translated
+  ['already_allowlisted'] = 'De speler heeft al een toegangspas op deze server!', --not translated
+  ['license'] = 'licentie', --not translated
+  ['help_license'] = 'de speler zijn licentie', --not translated
+  ['identifier_not_allowlisted'] = 'Deze identifier heeft geen toegangspas op deze server!', --not translated
 }
